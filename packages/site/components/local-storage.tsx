e8716c0941e6a9b89dd320964bfe7774d51294e3
Daniel Lauzon <daniel.lauzon@gmail.com>
2023-04-17 13:32:27 -0400

    Working Artifact registry/Cloud Run

---
+++ packages/site/components/local-storage.tsx
@@ -0,0 +1,45 @@
+
+import { useState, useEffect } from 'react';
+
+export function useLocalStorage(key, initialValue) {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value) => {
+    try {
+      const valueToStore = value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
+}
+
+// Not quite working yet: put this in an mdx file
+// import { ProjectIDInput } from "../../components/local-storage";
+// <ProjectIDInput />
+
+export function ProjectIDInput() {
+  const [name, setName] = useLocalStorage('projectID', 'pdcp-cloud-XXX-name');
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <p>Your ProjectID is: {name}!</p>
+    </div>
+  );
+}
