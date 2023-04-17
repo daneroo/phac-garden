43b9170b0e6daabe848c2a660661a81dfc7795b9
Daniel Lauzon <daniel.lauzon@gmail.com>
2023-04-17 14:51:08 -0400

    content-type fpr time-deno

--- apps/time-deno/server.ts
+++ apps/time-deno/server.ts
@@ -2,7 +2,14 @@ import { serve } from "https://deno.land/std@0.178.0/http/server.ts";
 const port = 8080;
 
 function handler(/* req: Request */): Response {
-  return new Response(JSON.stringify({ time: new Date() ,lang:"Deno/TypeScript"}));
+  return new Response(
+    JSON.stringify({ time: new Date(), lang: "Deno/TypeScript" }),
+    {
+      headers: {
+        "content-type": "application/json; charset=UTF-8",
+      },
+    }
+  );
 }
 
 serve(handler, { port });
